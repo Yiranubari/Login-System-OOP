e77ce4cef7603fd33936b781aba4bad78a043f9a
Yiranubari <yiranubari4@gmail.com>
2025-11-25 20:50:39 +0100

    add: create signup form in index.php and add Signup class skeleton

--- index.php
+++ index.php
@@ -8,7 +8,15 @@
 </head>
 
 <body>
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username">
+        <input type="password" name="pwd">
+        <button>Signup</button>
+    </form>
 
+    <?php
+
+    ?>
 </body>
 
 </html>

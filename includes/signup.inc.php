6516ba1865999a43a9b88537fab15b4f21f2f12c
Yiranubari <yiranubari4@gmail.com>
2025-11-26 09:58:19 +0100

    fix: correct require_once statement for Signup class to include .php extension

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pwd = $_POST['pwd'];
 
     require_once "../Classes/Dbh.php";
-    require_once "../Classes/Signup";
+    require_once "../Classes/Signup.php";
 
 
     $signup = new Signup($username, $pwd);
